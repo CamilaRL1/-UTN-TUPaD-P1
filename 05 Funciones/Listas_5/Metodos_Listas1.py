beebaac96ea9631caadab5a74c08ef54d17708d1
CamilaRL1 <rissicamila07@gmail.com>
2025-05-05 18:39:37 -0300

    Add files via upload

---
+++ 05 Funciones/Listas_5/Metodos_Listas1.py
@@ -0,0 +1,25 @@
+#Acrividad 1
+#multiples_de_4 = list(range(4, 101, 4))
+#rint(multiples_de_4)
+
+#Actividad 2
+#elementos = ["lectura", "música", "cine", "viajar", "baile"]
+#print(elementos[-2])  # Use el índice negativo para acceder al penúltimo
+
+#Actividad 3 
+#lista_vacia = []
+#lista_vacia.append("Agua")
+#lista_vacia.append("Tierra")
+#lista_vacia.append("Fuego")
+#print(lista_vacia)
+
+#Actividad 4
+#animales = ["perro", "gato", "conejo", "pez"]
+#animales[1] = "loro"  # Reemplaza el segundo elemento (índice 1)
+#animales[-1] = "oso"  # Reemplaza el último elemento (índice -1)
+#print(animales)
+
+#Actividad 5
+numeros = [8, 15, 3, 22, 7]#se crea una lista con valores
+numeros.remove(max(numeros))#se tiene que encontar el valor maximo de la lista y se elimina el primer elemento que es el valor 22
+print(numeros)#Se imprime la lista 
---
+++ 05 Funciones/Listas_5/Metodos_Listas2.py
@@ -0,0 +1,34 @@
+#Actividad 6
+#numeros = list(range(10, 31, 5))
+#print(numeros[:2]) 
+
+#Actividad 7
+#autos = ["sedan", "polo", "suran", "gol"]
+#autos[1] = "pinta"  # Reemplaza el segundo valor (índice 1)
+#autos[2] = "dieta"  # Reemplaza el tercer valor (índice 2)
+
+#Actividad 8
+#dobles = []
+#dobles.append(5 * 2)
+#dobles.append(10 * 2)
+#dobles.append(15 * 2)
+#print(dobles)
+
+#Actividad 9
+#compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+
+#compras[2].append("jugo")
+#compras[1][1] = "tallarines"
+#compras[0].remove("pan")
+
+#print(compras)
+
+#Actividad 10
+lista_anidada = [
+    15,
+    True,
+    [25.5, 57.9, 30.6],
+    False
+]
+print(lista_anidada)
+
